88ce53f55f3c17dfd2716291e6bc963894a6dfb9
Daniel74 <d.tshisuaka@gmail.com>
2026-05-05 14:43:02 +0200

    Updated models with password and hashed password.
    Added additional ModelError.

--- web_app/models.py
+++ web_app/models.py
@@ -2,6 +2,8 @@ from sqlmodel import SQLModel, Field
 from datetime import datetime, timezone
 from enum import Enum
 
+
+#--------------------------- User Models ----------------------------
 class UserBase (SQLModel):
     """
     A data model with the basic user attributes.
@@ -19,14 +21,14 @@ class User (UserBase, table=True):
     id: int | None = Field (default=None, primary_key=True) # need to be optional due to SQLModel inner workings.
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc)
     )
-    # hashed_password : str
+    hashed_password : str
 
 
 class UserCreate (UserBase):
     """
     A data model with the additional attributes during user creation.
     """
-    # password: str
+    password: str
     pass
 
 
@@ -49,10 +51,16 @@ class UserPatch (SQLModel):
     password: str | None = None
 
 
+#--------------------------- Model Errors ----------------------------
+
 class ModelError(str, Enum):
     """
     Collects the different error codes when working with table models.
     """
     VALIDATION_ERROR = "VALIDATION_ERROR" #errors caused by Pydantic evaluation.
+    DATABASE_ERROR = "DATABASE_ERROR" # general database errors
+
+    # Errors related to user CRUD
     USER_NAME_ALREADY_EXISTS = "USER_NAME_ALREADY_EXISTS"
-    DATABASE_ERROR = "DATABASE_ERROR"
+    USER_NAME_NOT_FOUND = "USER_NAME_NOT_FOUND"
+    USER_ID_NOT_FOUND = "USER_ID_NOT_FOUND"
