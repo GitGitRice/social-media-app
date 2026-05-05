dd1aebb65d69fdd52e930ca838b0b178b6a5d6c5
GitGitRice <steven_tanu@hotmail.com>
2026-05-05 19:07:38 +0200

    added import PostCreate, PostRead added functions:  add_post, get_posts, get_user_posts

--- console_app/client_api.py
+++ console_app/client_api.py
@@ -1,5 +1,5 @@
 import requests
-from web_app.models import UserCreate, UserRead
+from web_app.models import UserCreate, UserRead, PostCreate, PostRead
 
 BASE_URL = "http://127.0.0.1:8000/api"
 
@@ -26,6 +26,43 @@ def get_users() -> list[UserRead]:
         if response.status_code == 200:
             # validate JSON response into UserRead object
             return [UserRead.model_validate(u) for u in response.json()]
+        return []
+    except requests.exceptions.ConnectionError:
+        return []
+
+
+# POST
+def add_post(post: PostCreate, user_id: int) -> PostRead | None:
+    """Sendet einen neuen Post an den Server."""
+    try:
+        # user_id wird als Query-Parameter übergeben
+        response = requests.post(
+            f"{BASE_URL}/posts", 
+            params={"user_id": user_id}, 
+            json=post.model_dump()
+        )
+        if response.status_code == 200:
+            return PostRead.model_validate(response.json())
+        return None
+    except requests.exceptions.ConnectionError:
+        return None
+
+def get_posts(offset: int = 0, limit: int = 20) -> list[PostRead]:
+    """Holt den globalen Feed."""
+    try:
+        response = requests.get(f"{BASE_URL}/posts", params={"offset": offset, "limit": limit})
+        if response.status_code == 200:
+            return [PostRead.model_validate(p) for p in response.json()]
+        return []
+    except requests.exceptions.ConnectionError:
+        return []
+
+def get_user_posts(user_id: int) -> list[PostRead]:
+    """Holt alle Posts eines spezifischen Nutzers."""
+    try:
+        response = requests.get(f"{BASE_URL}/users/{user_id}/posts")
+        if response.status_code == 200:
+            return [PostRead.model_validate(p) for p in response.json()]
         return []
     except requests.exceptions.ConnectionError:
         return []
