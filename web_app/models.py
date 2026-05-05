0b287bdaa1a8c92bcb34693d4a6e4e7e28e1ab10
GitGitRice <steven_tanu@hotmail.com>
2026-05-05 19:05:37 +0200

    class PostBase hinzugefügt in line 6 class Post hinzugefügt in line 11 class User angepasst: Relationship(back_populates="author") in line 41 class PostCreate hinzugefügt in line 79 class PostRead hinzugefügt in line 82

--- web_app/models.py
+++ web_app/models.py
@@ -1,6 +1,24 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime, timezone
 from enum import Enum
+
+
+class PostBase(SQLModel):
+    content: str
+    # author_id ist der Fremdschlüssel zum User
+    author_id: int | None = Field(default=None, foreign_key="user.id")
+
+class Post(PostBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    # index=True für schnellere Abfragen beim Feed
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc), 
+        index=True
+    )
+    
+    # Relationship: Ein Post gehört zu einem Autor
+    author: "User" = Relationship(back_populates="posts")
+
 
 class UserBase (SQLModel):
     """
@@ -20,6 +38,7 @@ class User (UserBase, table=True):
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc)
     )
     # hashed_password : str
+    posts: list["Post"] = Relationship(back_populates="author")
 
 
 class UserCreate (UserBase):
@@ -56,3 +75,10 @@ class ModelError(str, Enum):
     VALIDATION_ERROR = "VALIDATION_ERROR" #errors caused by Pydantic evaluation.
     USER_NAME_ALREADY_EXISTS = "USER_NAME_ALREADY_EXISTS"
     DATABASE_ERROR = "DATABASE_ERROR"
+
+class PostCreate(PostBase):
+    pass
+
+class PostRead(PostBase):
+    id: int
+    created_at: datetime
