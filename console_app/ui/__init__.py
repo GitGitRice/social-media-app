8950faea253ae61ac76647e8b5364c21c754430b
GitGitRice <steven_tanu@hotmail.com>
2026-05-11 20:08:43 +0200

    remove double import,

--- console_app/ui/__init__.py
+++ console_app/ui/__init__.py
@@ -4,7 +4,6 @@ from .welcome import welcome_screen
 from .login import login_screen
 from .register import register_screen
 from .end_app import end_app_screen
-from .main_menu import main_menu_screen
 from .user_details import user_details_screen
 from .user_directory import user_directory_screen
 from .global_feed import global_feed_screen
