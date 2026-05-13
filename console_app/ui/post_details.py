bd4c05e44aeb086b0a70cbccb7419fdcd6647af8
Daniel74 <d.tshisuaka@gmail.com>
2026-05-13 07:51:00 +0200

    - enhanced global_feed_screen - implemented stuf for post_details_screen

---
+++ console_app/ui/post_details.py
@@ -0,0 +1,16 @@
+from rich.console import Console
+import questionary
+
+console = Console()
+
+def post_details_screen(selected_post_id):
+    console.print("Selected_post_id:", selected_post_id)
+    choice= questionary.select(
+        "",
+        choices=["Back", "Home"]
+    ).ask()
+
+    if choice == "Back":
+        return "BACK"
+    elif choice == "Home":
+        return "HOME"
