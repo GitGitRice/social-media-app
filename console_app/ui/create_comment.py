b224630d647f476c30938ae8560fc756e31e7a09
Daniel74 <d.tshisuaka@gmail.com>
2026-05-18 11:46:46 +0200

    create comments screen shows post content

--- console_app/ui/create_comment.py
+++ console_app/ui/create_comment.py
@@ -1,12 +1,23 @@
 from .utils import clear_screen, print_header, print_error, print_success
 import questionary
-from console_app.client_api import create_comment
+from console_app.client_api import create_comment, get_post
 
+from rich import print
+from rich.panel import Panel
+      
 def create_comment_screen (selected_post_id: int):
 
     # clear screen and print header
     clear_screen()
     print_header("Social Media App", "Comment Post")
+
+    # show post content
+    selected_post = get_post(selected_post_id)
+    if not selected_post:
+        print_error("Post could not be retrieved")
+        return "BACK"
+    
+    print(Panel(selected_post.content))
 
     # collect comment
     comment = questionary.text("", multiline=True).ask()
